dabe177794d035b2cb20679d43f0a791ea91cc63
linwenjun <linwenjun@163.com>
2020-02-25 08:15:54 +0000

    first commit

---
+++ annotation/src/main/java/com/thoughtworks/model/Student.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.model;
+
+// 为Student类添加一个Alias注解, 表明Student类可以取一个自定义的别名
+public class Student {
+
+    // 为age字段添加一个Limit注解, 表明age字段的最小值为1 最大值为140
+    private int age;
+
+}
