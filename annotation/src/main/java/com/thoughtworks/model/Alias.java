919e85752632e73723a7a0b64b5e503758ecc7c6
zkj <438101845@qq.com>
2020-02-27 13:36:04 +0800

    {zkj}-finished

---
+++ annotation/src/main/java/com/thoughtworks/model/Alias.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.model;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Alias {
+    //可以取别名
+    String value() default "another name";
+}
--- annotation/src/main/java/com/thoughtworks/model/Student.java
+++ annotation/src/main/java/com/thoughtworks/model/Student.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.model;
 
 // 为Student类添加一个Alias注解, 表明Student类可以取一个自定义的别名
+@Alias("PrivateStudent")
 public class Student {
 
     // 为age字段添加一个Limit注解, 表明age字段的最小值为1 最大值为140
+    @Limit(min = 1, max = 140)
     private int age;
 
 }
